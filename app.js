f6d398809096dd170d1576c3ddb078560b94f9ab
GoAddley <ashl3y_n@hotmail.com>
2022-01-24 11:19:53 +1000

    Stylized for all screens. Added error message if input is left blank. Set timer for error to hide

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 const form = document.querySelector('.container');
 const ul = document.getElementById('list');
 const val = document.querySelector('.input');
+const inner = document.querySelector('.inner');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    //div
-    const div = document.createElement('div');
-    div.classList.add('innderDiv');
-    //Create new li for list
-    const li = document.createElement('li');
-    li.textContent = val.value;
-    li.classList.add('list-item');
-    div.appendChild(li);
-    //Create Remove button
-    const delBtn = document.createElement('button');
-    delBtn.textContent = 'Remove';
-    delBtn.classList.add('delBtn');
-    li.appendChild(delBtn);
-    val.value = '';
-    //remove button to delete items
-    delBtn.addEventListener('click', () =>{
-        li.remove();
-        delBtn.remove();
-    });
-    //Append to list
-    ul.appendChild(div);
+    //Condition to see if input field has been filled in
+    if(val.value === ''){
+        const error = document.createElement('h3');
+        error.classList.add('error');
+        error.innerHTML = 'You need to add something!';
+        error.style.color = '#ffffff';
+        console.log('Works!');
+        inner.appendChild(error);
+        //set timer for error message to hide
+        setInterval(() => {
+            error.style.display = 'none';
+        }, 1500);
+    } else {
+        const div = document.createElement('div');
+        div.classList.add('innderDiv');
+        //Create new li for list
+        const li = document.createElement('li');
+        li.textContent = val.value;
+        li.classList.add('list-item');
+        div.appendChild(li);
+        //Create Remove button
+        const delBtn = document.createElement('button');
+        delBtn.textContent = 'Remove';
+        delBtn.classList.add('delBtn');
+        li.appendChild(delBtn);
+        val.value = '';
+        //remove button to delete items
+        delBtn.addEventListener('click', () =>{
+            li.remove();
+            delBtn.remove();
+        });
+        //Append to list
+        ul.appendChild(div);
+        }
 });
 
